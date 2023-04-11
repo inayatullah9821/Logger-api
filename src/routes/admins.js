5d33934aff4ef77f0f527308ac55f5679936e9bc
inayatullahaaxioned <inayatullaha@axioned.com>
2023-04-11 16:00:18 +0530

    admin user crud route added

--- src/routes/admins.js
+++ src/routes/admins.js
@@ -3,11 +3,14 @@ const { authenticate, checkRole } = require('../middleware/authentication');
 
 const {
   createUser,
+  deleteUser
 } = require('../controller/admins');
 
 
 router.use([authenticate, checkRole]);
 
 router.post('/', createUser);
+
+router.delete('/:userId', deleteUser);
 
 module.exports = router;
